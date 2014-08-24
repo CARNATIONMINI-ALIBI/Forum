9cb57bce225e96e0dfa9568e0a0d41595ff139e0
goshow <georgiinkov@gmail.com>
2014-08-24 19:58:16 +0300

    topics view

---
+++ Views/topics/view.php
@@ -0,0 +1,31 @@
+<section class="viewTopic">
+    <header>
+        <h1>Title of the topic</h1>
+        <a href="#" class="postReply">Postreply</a>
+        <button id="editTopic">Edit</button>
+        <button id="closeTopic">Close</button>
+        <button id="deleteTopic">Delete</button>
+    </header>
+    <div id="myAnswer">
+        <p>Lorem Ipsum dsfijl dsfij lsdfi sdlifj ldsifj ldsifj ldsifj </p>
+        <p>Lorem Ipsum dsfijl dsfij lsdfi sdlifj ldsifj ldsifj ldsifj </p>
+        <p>Lorem Ipsum dsfijl dsfij lsdfi sdlifj ldsifj ldsifj ldsifj </p>
+        <p>Lorem Ipsum dsfijl dsfij lsdfi sdlifj ldsifj ldsifj ldsifj </p>
+        <p>Lorem Ipsum dsfijl dsfij lsdfi sdlifj ldsifj ldsifj ldsifj </p>
+        <p>Lorem Ipsum dsfijl dsfij lsdfi sdlifj ldsifj ldsifj ldsifj </p>
+    </div>
+    <div class="otherAnswer">
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+    </div>
+    <div class="otherAnswer">
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+        <p>Other Answer Lorem Ipsum dyrabababab aroijdo fdosifj sdoifj sdoif j fdslkjf sdlkfj sdlkj </p>
+    </div>
+</section>
