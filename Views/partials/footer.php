a1b5a4d241dfa3add91b2bcd94e8661ed80a8b34
goshow <georgiinkov@gmail.com>
2014-08-25 15:56:37 +0300

    who is online

--- Views/partials/footer.php
+++ Views/partials/footer.php
@@ -1,6 +1,13 @@
 </main>
 <footer>
-    <p>Powered By ANSR Framework</p>
+    <section class="whoIsOnline">
+        <a href="../users/online.php"><h3>Who is Online </h3></a>
+        <div>
+            <p>Our users have posted <span>25</span> articles</p>
+            <p>The newest registered user is <a href="#">RoYaLL.</a></p>
+        </div>
+    </section>
+    <h4>Powered By ANSR Framework</h4>
 </footer>
 </div>
 </body>
---
+++ Views/users/online.php
@@ -0,0 +1,12 @@
+<table class="online">
+    <tr>
+        <th>Usernam</th>
+        <th>Last Updated</th>
+        <th>Forum Location</th>
+    </tr>
+    <tr>
+        <td>Royall</td>
+        <td>25 Aug 2014 03:47 PM</td>
+        <td>Forum Index</td>
+    </tr>
+</table>
