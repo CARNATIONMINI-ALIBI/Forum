73cec5dd3cae988149b2127b82f05fdcdd7d32eb
RoYaLBG <royal4o@gmail.com>
2014-08-26 21:42:33 +0300

    Fixing broken commit 55156cbf

--- Views/topics/add.php
+++ Views/topics/add.php
@@ -1,3 +1,18 @@
+    <script>
+        function addTopic() {
+            $.post("", {
+                summary: $('#summary').val(),
+                body: $('#body').val(),
+                tags: $('#tags').val()
+            }).done(function (response) {
+                var json = $.parseJSON(response);
+                if (json.success == 1) {
+                    $("#response").html("<h1>Topic has been added successfully</h1>");
+                    window.location = "<?= $this->url('topics', 'view', 'id');?>" + json.topic_id
+                }
+            });
+        }
+    </script>
     <div id="addTopic">
         <div id="response"><h2>New Topic</h2></div>
         <input type="text" name="summary" id="summary" placeholder="Summary"/>
@@ -5,19 +20,5 @@
         <input type="text" id="tags" placeholder="tags"/>
         <button onclick="addTopic()">Add topic</button>
     </div>
-    <script>
-
-        function addTopic() {
-            $.post("<?=$this->url('topics', 'add', 'forumid', 1);?>", {
-                summary: $('#summary').val(),
-                body: $('#body').val()
-            }).done(function (response) {
-                var json = $.parseJSON(response);
-                if (json.success == 1) {
-                    $("#response").html("<h2>Topic has been added successfully</h2>");
-                }
-            });
-        }
-    </script>
 
 
