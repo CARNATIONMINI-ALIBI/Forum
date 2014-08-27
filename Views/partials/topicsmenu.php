0fbe809b9ebea351a8b029361a93999a4883907f
RoYaL <royal4o@gmail.com>
2014-08-27 15:41:56 +0300

    minor fixes

--- Views/partials/topicsmenu.php
+++ Views/partials/topicsmenu.php
@@ -1,7 +1,8 @@
-<?php if (!isset($this->topic)) die; ?>
+<?php if (isset($this->topic)): ?>
 
 <?php $forum = $this->getFrontController()->getController()->getApp()->ForumModel->getForumById($this->topic['forum_id']); ?>
 <li>
     <li><a href="<?= $this->url('forums', 'view', 'id', $this->topic['forum_id']); ?>"><?= $forum['name']; ?></a><span> -></span></li>
     <li><a href="<?= $this->url('topics', 'view', 'id', $this->topic['id']); ?>"><?= $this->topic['summary']; ?></a><span></span></li>
 </li>
+<?php endif; ?>
